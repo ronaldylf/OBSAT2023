8e41413ec6ef764504ab4be8cbc1e9d76ad7128a
Ronaldy <ronaldylf@gmail.com>
2023-08-01 15:49:49 -0300

    main files

---
+++ CubeSat.py
@@ -0,0 +1,148 @@
+from machine import (
+    Pin,
+    ADC,
+    SoftI2C,
+    SoftSPI,
+    PWM,
+    reset
+)
+from imu import MPU6050
+from time import sleep
+from bmp280 import *
+import network
+import os
+from sdcard import SDCard
+import aht
+from myENS160 import myENS160
+
+class CubeSat:
+    def __init__(self):
+        self.battery_level = 0
+        self.i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
+        self.spi = SoftSPI(-1,
+              miso=Pin(19),
+              mosi=Pin(23),
+              sck=Pin(18)
+        )
+        self.cs = Pin(5, Pin.OUT)
+        self.buzzer_gpio = 32
+        self.battery_gpio = 35
+    
+    def reset(self):
+        reset()
+    
+    def wifi_connect(self, ssid="", password=""):
+        sta_if = network.WLAN(network.STA_IF)
+        if not sta_if.isconnected():
+            print(f"trying to connect in '{ssid}'")
+            sta_if.active(True)
+            sta_if.connect(ssid, password)
+            while not sta_if.isconnected():
+                pass
+        print(f"connection OK with '{ssid}'")
+    
+    def mount_sd(self):
+        self.sd = SDCard(self.spi, self.cs)
+        os.mount(self.sd, '/sd')
+    
+    def clear_sd(self): # limpar arquivos do cartão micro SD
+        for file in os.listdir('/sd'):
+            if '.' in file: os.remove(f'/sd/{file}')
+
+    def read_file(self, file_name): # lê o conteúdo de algum arquivo de texto
+        with open(file_name, 'r+') as file: return file.read()
+
+    def init_buzzer(self, freq=2000, duty=512): # inicia buzzer
+        self.beeper = PWM(Pin(self.buzzer_gpio, Pin.OUT), freq=freq, duty=duty)
+
+    def deinit_buzzer(self): # interrompe buzzer
+        self.beeper.deinit()
+
+    def beep(self, on_time=0.1, freq=4000, duty=512): # soltar bipes como indicação
+        self.init_buzzer(freq, duty)
+        sleep(on_time)
+        self.deinit_buzzer()
+    
+    def get_battery_level(self): # nível da bateria em porcentagem (%)
+        pot = ADC(Pin(self.battery_gpio, mode=Pin.IN))
+        pot.atten(ADC.ATTN_11DB)
+        self.battery_level = round((pot.read()/4095)*1.09*100, 2)
+        return self.battery_level
+    
+    def gyroscope(self): # leitura do giroscópio
+        try:
+            self.mpu = MPU6050(self.i2c)
+            return [
+                round(self.mpu.gyro.x, 2),
+                round(self.mpu.gyro.y, 2),
+                round(self.mpu.gyro.z, 2)
+            ]
+
+        except Exception as e:
+            print(e)
+            return [-1, -1, -1]
+
+    def acceleration(self): # leitura do acelerômetro
+        try:
+            self.mpu = MPU6050(self.i2c)
+            return [
+                round(self.mpu.accel.x, 2),
+                round(self.mpu.accel.y, 2),
+                round(self.mpu.accel.z, 2)
+            ]
+        except Exception as e:
+            print(e)
+            return [-1, -1, -1]
+
+    def get_angle(self): # pegar ângulo do satélite
+        import math
+        self.angle = math.asin(self.acceleration()[1]/9.8)
+        return self.angle
+    
+    def temperature(self, sensor=0): # temperatura em graus celsius
+        '''
+        sensor=0: lida pelo sensor mpu6050
+        sensor=1: lida pelo sensor bmp280
+        sensor=2: lida pelo sensor aht21
+        '''
+        try:
+            self.temperature_ = None
+            if (sensor==0):
+                self.bmp = BMP280(self.i2c)
+                self.bmp.use_case(BMP280_CASE_INDOOR)
+                self.temperature_ = self.bmp.temperature
+            elif (sensor==1):
+                self.mpu = MPU6050(self.i2c)
+                self.temperature_ = self.mpu.temperature
+            else:
+                self.aht_sensor = aht.AHT2x(self.i2c, crc=True)
+                self.temperature_ = self.aht_sensor.temperature
+            return round(self.temperature_, 2)
+        except Exception as e:
+            print(e)
+            return -1
+        
+    def pressure(self): # pressão em pascal
+        try:
+            self.bmp = BMP280(self.i2c)
+            self.bmp.use_case(BMP280_CASE_INDOOR)
+            return self.bmp.pressure
+        except Exception as e:
+            print(e)
+            return -1
+    
+    def humidity(self): # umidade em porcentagem (%)
+        try:
+            self.aht_sensor = aht.AHT2x(self.i2c, crc=True)
+            return round(self.aht_sensor.humidity, 2)
+        except Exception as e:
+            print(e)
+            return -1
+        
+    def get_air_quality(self): # leitura dos dados de qualidade do ar
+        self.ens = myENS160(self.i2c)
+        return {
+            'TVOC': self.ens.getTVOC(),
+            'AQI': self.ens.getAQI(),
+            'ECO2': self.ens.getECO2()
+        }
---
+++ boot.py
@@ -0,0 +1,67 @@
+from CubeSat import CubeSat
+from time import sleep, time
+import urequests
+import json
+import network
+
+print("turning on...")
+sat = CubeSat() # inicia a classe do satélite
+sat.beep(freq=5000)
+sat.wifi_connect(ssid="MAKE_CURSINHO", password="nossainternet") #conecta a rede wi-fi
+sat.beep(freq=5000)
+sat.mount_sd() # monta cartão microSD
+print("sd mounted")
+sat.beep(freq=5000)
+
+# variáveis editáveis
+server = "https://obsat.org.br/teste_post/envio.php" # servidor de envio dos dados via requisição POST
+min_interval = 1 # segundos entre cada execução
+exec_num = 0
+
+while True:
+    print("waiting execution... ")
+    start_time = time()
+    while((time()-start_time)<min_interval): pass # aguarda o intervalo entre as execuções
+    sat.beep()
+    exec_num+=1
+    print(f"execução número {exec_num}")
+    
+    # captura dos dados de telemetria e payload
+    telemetry = {
+        'equipe': 33,
+        'bateria': sat.get_battery_level(),
+        'temperatura': sat.temperature(),
+        'pressao': sat.pressure(),
+        'giroscopio': sat.gyroscope(),
+        'acelerometro': sat.acceleration(),
+        'payload': {
+            'umidade': sat.humidity(),
+            'time': time(),
+
+        },
+    }
+    telemetry['payload'].update(sat.get_air_quality()) # adiciona ao payload os dados dos gases
+    json_telemetry = json.dumps(telemetry) # conversão para o padrão json
+    print("json_telemetry:", json_telemetry)
+    
+    # salva no cartão microSD
+    with open(f'/sd/telemetry.json', 'a+') as file:
+        file.write(json_telemetry+"\n")
+        print("saved on sd card")
+
+    #envia os dados via wi-fi
+    print("sending packet...")
+    try:
+        response = urequests.post(url=server, data=json_telemetry)
+        response.close()
+        print(f"{response.status_code} packet sent to {server}")
+    except Exception as e:
+        print("something bad happened:", e)
+        sta_if = network.WLAN(network.STA_IF)
+        print(f"sta_if.isconnected(): {sta_if.isconnected()}")
+        for i in range(10): sat.beep(); sleep(0.1)
+    except KeyboardInterrupt as e:
+        raise e
+
+    sat.beep(freq=2000)
+    print("="*120)
